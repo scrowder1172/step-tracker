ae56d8267d9eefc92f63e4544e5c8aae8df95117
Scott Crowder <scrowder@gmail.com>
2024-08-14 09:26:49 -0500

    created xCode project

---
+++ StepTracker/StepTracker/StepTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+// File: StepTrackerApp.swift
+// Project: StepTracker
+// 
+// Created by SCOTT CROWDER on 8/14/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
