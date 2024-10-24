a18798e48efe9fb9cea9db110969d6e4c37ac3ad
Scott Crowder <scrowder@gmail.com>
2024-10-24 11:08:31 -0500

    pie chart interactivity

--- StepTracker/StepTracker/Charts/StepPieChart.swift
+++ StepTracker/StepTracker/Charts/StepPieChart.swift
@@ -12,7 +12,20 @@ import SwiftUI
 
 struct StepPieChart: View {
     
+    @State private var rawSelectedChartValue: Double? = 0
+    
     var chartData: [WeekdayChartData]
+    
+    var selectedWeekday: WeekdayChartData? {
+        guard let rawSelectedChartValue else { return nil }
+        
+        var total: Double = 0.0
+        
+        return chartData.first {
+            total += $0.value
+            return rawSelectedChartValue <= total
+        }
+    }
     
     var body: some View {
         VStack(alignment: .leading){
@@ -31,13 +44,36 @@ struct StepPieChart: View {
                     SectorMark(
                         angle: .value("Average Steps", weekday.value),
                         innerRadius: .ratio(0.618),
+                        outerRadius: selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 140 : 110,
                         angularInset: 1
                     )
                     .foregroundStyle(.pink.gradient)
                     .cornerRadius(6)
+                    .opacity(selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 1.0 : 0.3)
                 }
             }
+            .chartAngleSelection(value: $rawSelectedChartValue)
             .frame(height: 240)
+            .chartBackground { proxy in
+                GeometryReader { geo in
+                    if let plotFrame = proxy.plotFrame {
+                        let frame = geo[plotFrame]
+                        if let selectedWeekday {
+                            VStack {
+                                Text(selectedWeekday.date.weekdayTitle)
+                                    .font(.title3)
+                                    .contentTransition(.identity)
+                                
+                                Text(selectedWeekday.value, format: .number.precision(.fractionLength(0)))
+                                    .fontWeight(.medium)
+                                    .foregroundStyle(.secondary)
+                                    .contentTransition(.numericText())
+                            }
+                            .position(x: frame.midX, y: 10.0)
+                        }
+                    }
+                }
+            }
         }
         .padding()
         .background(
